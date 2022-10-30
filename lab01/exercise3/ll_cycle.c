ff585a4fde2efb9f5a30b24b8f602f226a46d08a
misaka <1784321894@qq.com>
2022-10-30 14:32:13 +0800

    CS61C Lab01: Execise3 ll_cycle

--- lab01/exercise3/ll_cycle.c
+++ lab01/exercise3/ll_cycle.c
@@ -3,4 +3,20 @@
 
 int ll_has_cycle(node *head) {
     /* TODO: Implement ll_has_cycle */
+    if(head == NULL) {
+        return 0;
+    }
+
+    node *fast_ptr = head;
+    node *slow_ptr = head;
+
+    while(fast_ptr && fast_ptr -> next && fast_ptr -> next -> next) {
+        fast_ptr = fast_ptr -> next -> next;
+        slow_ptr = slow_ptr -> next;
+
+        if(fast_ptr == slow_ptr) {
+            return 1;
+        }
+    }
+    return 0;
 }
