64dd451ef609e342bc8143c29741e8a7b91e6968
misaka <1784321894@qq.com>
2022-11-05 22:25:24 +0800

    CS61C Lab03: Exercise 5 Factorial

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -27,7 +27,24 @@ main:
 # The return value should be stored in a0
 factorial:
     # YOUR CODE HERE
-
+    # a0 = n
+    addi t0, x0, 1 # res = 1
+    addi t3, x0, 1 # flag = 1
+loop_outer:
+    beq a0, x0, finish_outer # n > 0
+    addi a1, a0, -1 # i = n - 1
+    add t1, x0, x0 # tmp = 0
+loop_inner:
+    beq a1, x0, finish_inner # i > 0
+    add t1, t1, t0 # tmp = tmp + res
+    addi a1, a1, -1 # i -- 
+    j loop_inner
+finish_inner:
+    add t0, t0, t1 # res = res + tmp
+    addi a0, a0, -1 # n -- 
+    j loop_outer
+finish_outer:
+    add a0, t0, x0 # return res
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
     jr ra
