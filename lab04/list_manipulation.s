f549f78759e448d71aa2e00101e4f6ab1506d3d1
misaka <1784321894@qq.com>
2022-11-08 19:29:51 +0800

    CS61C Lab04: Exercise 3 List Manipulation

--- lab04/list_manipulation.s
+++ lab04/list_manipulation.s
@@ -47,9 +47,16 @@ main:
 update_list:
     # TODO: which registers do we need to save? (complete this after you have finished the other todos because
     # we don't know which registers we need to save until we implement the function)
+    addi sp, sp, -12
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
 
     # TODO: implement if (!curr_node) { return; }
+    bne a0, x0, update_list_loop_recurse
+    j done
 
+update_list_loop_recurse:
     add s0, a0, x0      # save address of the current node in s0
     add s1, a1, x0      # save address of function in s1
     add t0, x0, x0      # t0 is a counter that we will used to know when we have reached the end of the array
@@ -64,26 +71,43 @@ update_list:
     # we call. This is to enforce the abstraction barrier of calling convention.
 update_list_loop:
     # TODO: load the address of the array of current node into t1
-
+    lw t1, 0(s0)
     # TODO: load the size of the node's array into t2
-
+    lw t2, 4(s0)
     # TODO: load the value of the current number that we want to square into a0
     # Remember that t1 = address of array and t0 = index we are modifying
+    slli t3, t0, 2
+    add t4, t3, t1
+    lw a0, 0(t4)
 
     # TODO: which registers do we need to save before calling square?
     # (do this after implementing the rest of update_list_loop)
+    addi sp, sp, -20
+    sw t0, 0(sp)
+    sw t1, 4(sp)
+    sw t2, 8(sp)
+    sw t3, 12(sp)
+    sw t4, 16(sp)
 
     jalr s1             # call the function on that value.
 
     # TODO: restore the registers we saved before calling square
     # (do this after implementing the rest of update_list_loop)
+    lw t0, 0(sp)
+    lw t1, 4(sp)
+    lw t2, 8(sp)
+    lw t3, 12(sp)
+    lw t4, 16(sp)
+    addi sp, sp, 20
 
     # TODO: store the value returned by square back into the array
+    sw a0, 0(t4)
 
     addi t0, t0, 1               # increment the count
     bne t0, t2, update_list_loop # repeat if we haven't reached the array size yet
 
     #TODO: load the address of the next node into a0
+    lw a0, 8(s0)
     
     mv a1, s1       # put the address of the function back into a1 to prepare for the recursion
 
@@ -91,6 +115,10 @@ update_list_loop:
 done:
     # EPILOUGE
     # TODO: restore the registers that we saved at the beginning of update_list
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    addi sp, sp, 12
 
     jr ra
 
