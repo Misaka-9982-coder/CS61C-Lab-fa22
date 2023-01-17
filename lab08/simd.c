7a66a04672cd7c3777d45a7427f713be83dc98ca
misaka <1784321894@qq.com>
2023-01-17 13:00:44 +0800

    CS61C Lab08: Exercise 3 Writing SIMD Code

--- lab08/simd.c
+++ lab08/simd.c
@@ -53,8 +53,25 @@ long long int sum_simd(int vals[NUM_ELEMS]) {
 
     for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         /* YOUR CODE GOES HERE */
+        unsigned int i;
+        __m128i sum_vec = _mm_setzero_si128();
+
+        for (i = 0; i < NUM_ELEMS / 4 * 4; i += 4) {
+            __m128i tmp = _mm_loadu_si128((__m128i *) (vals + i));
+            __m128i mask = _mm_cmpgt_epi32(tmp, _127);
+            sum_vec = _mm_add_epi32(sum_vec, _mm_and_si128(tmp, mask));
+        }
+
+        unsigned int tmp_arr[4] = {0, 0, 0, 0};
+        _mm_storeu_si128((__m128i *) tmp_arr, sum_vec);
+        result += tmp_arr[0] + tmp_arr[1] + tmp_arr[2] + tmp_arr[3];
 
         /* Hint: you'll need a tail case. */
+        for (i = NUM_ELEMS / 4 * 4; i < NUM_ELEMS; i++) {
+            if (vals[i] >= 128) {
+                result += vals[i];
+            }
+        }
     }
 
     /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
